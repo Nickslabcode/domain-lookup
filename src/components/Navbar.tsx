49425f27192a5f54370b42fcbd4a44777f46550f
Nikola Nenovski <nicknenovski@gmail.com>
2024-11-09 13:52:04 +0200

    chore: updated navbar layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,22 +4,19 @@ import { IoReturnDownBackSharp } from 'react-icons/io5';
 
 const Navbar = () => {
   return (
-    <div className="flex items-center gap-2 py-4">
+    <div className="flex items-center gap-4 py-4 justify-around">
       <div className="flex">
         <div className="font-semibold text-2xl flex items-center gap-1">
           <RiSearchLine strokeWidth={1} className="text-primary" />
-          <h1>DomainLookup</h1>
-        </div>
-        <div className="flex">
-          <ThemeController />
+          <h1 className="text-center">DomainLookup</h1>
         </div>
       </div>
-      <div className="flex gap-1">
-        <label className="flex items-center input input-sm input-bordered">
+      <div className="flex gap-1 flex-1">
+        <label className="flex items-center input input-sm input-bordered w-full">
           <input
             type="text"
             className="grow"
-            placeholder="Search domain"
+            placeholder="Search for a domain..."
             // onChange={event => setDomain(event.target.value)}
           />
           <kbd className="kbd kbd-sm flex gap-1">
@@ -34,6 +31,9 @@ const Navbar = () => {
         >
           Accio!
         </button>
+      </div>
+      <div>
+        <ThemeController />
       </div>
     </div>
   );
