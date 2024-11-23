484a6356ea5c56f763cb492c97373904af48c53f
Nikola Nenovski <nicknenovski@gmail.com>
2024-11-23 12:08:35 +0200

    refactor(components): replaced h1 elements with H1 reusable higher order components
    Implemented cn helper function integrating clsx and tailwind-merge libraries to help create better
    reusable components. Replaced h1 elements with H1 components

--- src/components/SslTable.tsx
+++ src/components/SslTable.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { H1 } from '../hoc/H1';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const SslTable: React.FC<{ content: Record<string, any> }> = ({ content }) => {
   return (
     <div className="flex h-full max-h-screen flex-col break-words shadow-md p-4 rounded-lg cursor-default overflow-y-auto">
       {!content ? (
-        <h1>No SSL certificate found.</h1>
+        <H1>No SSL certificate found.</H1>
       ) : (
         <table className="table table-xs">
           <tbody>
