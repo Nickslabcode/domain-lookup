b29569d7d237e69005ffa0d10b2a9fb8f6260d15
Nikola Nenovski <nicknenovski@gmail.com>
2024-11-23 10:48:53 +0200

    fix(home): fix searchParams reset when going back to Home from Results view
    Add useEffect to reset the searchParams object when Home view is mounted

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,8 +2,16 @@ import ViewContainer from '../hoc/ViewContainer';
 import Footer from '../components/Footer';
 import SearchForm from '../components/SearchForm';
 import Logo from '../components/Logo';
+import { useSearchParams } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const Home = () => {
+  const [_searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    setSearchParams({});
+  }, [setSearchParams]);
+
   return (
     <>
       <ViewContainer>
