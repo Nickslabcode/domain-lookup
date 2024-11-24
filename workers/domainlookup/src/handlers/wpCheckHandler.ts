59704e6e23c5c1b189816bc50bc4afc1c89487b6
Nikola Nenovski <nicknenovski@gmail.com>
2024-11-24 16:32:53 +0200

    feat(cloudflare worker): add /wp-check endpoint
    Add endpoint to check whether WordPress is installed on the domain or not

---
+++ workers/domainlookup/src/handlers/wpCheckHandler.ts
@@ -0,0 +1,32 @@
+export const wpCheck = async (request: Request): Promise<Response> => {
+	const { searchParams } = new URL(request.url);
+	const domain = searchParams.get('domain');
+
+	if (!domain) {
+		return new Response('Domain query parameter is required', { status: 400 });
+	}
+
+	const headers = {
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'GET',
+		'Access-Control-Allow-Headers': 'Content-Type',
+	};
+
+	try {
+		const response = await fetch(`http://${domain}/readme.html`, {
+			method: 'HEAD',
+		});
+
+		if (response.ok) {
+			return new Response(JSON.stringify({ installed: true }), {
+				status: 200,
+				headers: { ...headers, 'Content-Type': 'application/json' },
+			});
+		} else {
+			return new Response('There was a problem fetching the data. Please try again later.', { status: response.status });
+		}
+	} catch (error) {
+		console.error('There was an error fetching the data: ', error);
+		return new Response('Internal server error. Please try again later.', { status: 500 });
+	}
+};
