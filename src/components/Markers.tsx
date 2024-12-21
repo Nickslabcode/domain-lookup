2abe09ffab6f57a98e480b9c27475d64b8d37125
Nikola Nenovski <nicknenovski@gmail.com>
2024-12-21 15:45:06 +0200

    feat(markers): add domain status and transfer checks

--- src/components/Markers.tsx
+++ src/components/Markers.tsx
@@ -1,6 +1,8 @@
 import { FaCheck } from 'react-icons/fa';
 import { IoClose } from 'react-icons/io5';
 import { DnsRecordAnswer } from '../types/DnsRecordAnswer';
+import { useMemo } from 'react';
+import NotAvailableMarker from './NotAvailableMarker';
 
 interface MarkersPropsType {
   AAAA: DnsRecordAnswer[] | string | undefined;
@@ -8,6 +10,7 @@ interface MarkersPropsType {
   hasWwwRecord: boolean;
   dnssec: string | undefined;
   wwwSsl: boolean;
+  domainStatusCodes: string[] | string;
 }
 
 const Markers: React.FC<MarkersPropsType> = ({
@@ -16,45 +19,74 @@ const Markers: React.FC<MarkersPropsType> = ({
   hasWwwRecord,
   dnssec,
   wwwSsl,
+  domainStatusCodes,
 }) => {
+  const transferCheck = useMemo(() => {
+    if (!domainStatusCodes) return;
+
+    if (domainStatusCodes.includes('ok')) return 'active';
+    else if (domainStatusCodes.includes('pendingTransfer'))
+      return 'pending transfer';
+    else return 'locked';
+  }, [domainStatusCodes]);
+
+  const domainStatus = useMemo(() => {
+    if (!domainStatusCodes) return;
+
+    const statuses: string[] | string = Array.isArray(domainStatusCodes)
+      ? domainStatusCodes.map((status: string) =>
+          status.substring(0, status.indexOf(' '))
+        )
+      : domainStatusCodes.substring(0, domainStatusCodes.indexOf(' '));
+
+    if (statuses.includes('clientHold')) return 'hold';
+    else if (statuses.includes('pendingDelete')) return 'pending delete';
+    else if (statuses.includes('redemptionPeriod')) return 'redemption';
+    else if (statuses.includes('inactive')) return 'inactive';
+    else if (statuses.includes('pendingUpdate')) return 'pending update';
+    else return 'active';
+  }, [domainStatusCodes]);
+
   return (
     <div className="mb-5 bg-base-200 py-2 px-4 rounded-lg text-secondary cursor-default">
       <ul className="flex gap-6 text-xs items-center">
         <li className="flex items-center gap-2">
           <h3 className="font-semibold">NO AAAA</h3>
-          {Array.isArray(AAAA) ? (
-            <div className="bg-error text-neutral w-4 h-4 rounded-full font-semibold flex justify-center items-center">
-              <IoClose size={12} />
-            </div>
-          ) : typeof AAAA === 'string' ? (
-            <div className="bg-success text-neutral w-4 h-4 rounded-full font-semibold flex justify-center items-center">
-              <FaCheck size={8} />
-            </div>
+          {!domainStatus ? (
+            <NotAvailableMarker />
           ) : (
-            <span className="bg-secondary text-neutral font-semibold text-xs px-1 rounded">
-              N/A
+            <span
+              className={`${
+                Array.isArray(AAAA) ? 'bg-error' : 'bg-success'
+              } text-neutral w-4 h-4 rounded-full font-semibold flex justify-center items-center`}
+            >
+              {Array.isArray(AAAA) ? (
+                <IoClose size={12} />
+              ) : (
+                <FaCheck size={8} />
+              )}
             </span>
           )}
         </li>
         <li className="flex items-center gap-2">
           <h3 className="font-semibold">WWW DNS</h3>
-          {hasWwwRecord ? (
-            <div className="bg-success text-neutral w-4 h-4 rounded-full font-semibold flex justify-center items-center">
-              <FaCheck size={8} />
-            </div>
+          {!domainStatus ? (
+            <NotAvailableMarker />
           ) : (
-            <div className="bg-error text-neutral w-4 h-4 rounded-full font-semibold flex justify-center items-center">
-              <IoClose size={12} />
-            </div>
+            <span
+              className={`${
+                hasWwwRecord ? 'bg-success' : 'bg-error'
+              } text-neutral w-4 h-4 rounded-full font-semibold flex justify-center items-center`}
+            >
+              {hasWwwRecord ? <FaCheck size={8} /> : <IoClose size={12} />}
+            </span>
           )}
         </li>
         <div className="bg-secondary w-0.5 h-3 rounded-full"></div>
         <li className="flex items-center gap-2">
           <h3 className="font-semibold">DNSSEC</h3>
           {!dnssec ? (
-            <span className="bg-secondary text-neutral font-semibold text-xs px-1 rounded">
-              N/A
-            </span>
+            <NotAvailableMarker />
           ) : dnssec === 'unsigned' ? (
             <span className="bg-success text-neutral font-semibold text-xs px-1 rounded">
               unsigned
@@ -67,45 +99,87 @@ const Markers: React.FC<MarkersPropsType> = ({
         </li>
         <li className="flex gap-2">
           <h3 className="font-semibold">STATUS</h3>
-          <span className="bg-success text-neutral font-semibold text-xs px-1 rounded">
-            active
-          </span>
+          <div className="dropdown dropdown-hover">
+            {!domainStatus ? (
+              <span className="bg-secondary text-neutral font-semibold text-xs px-1 rounded">
+                N/A
+              </span>
+            ) : (
+              <span
+                tabIndex={0}
+                role="button"
+                className={`${
+                  domainStatus === 'active' ? 'bg-success' : 'bg-error'
+                } text-neutral font-semibold text-xs px-1 rounded`}
+              >
+                {domainStatus}
+              </span>
+            )}
+            <a
+              tabIndex={0}
+              className="dropdown-content menu bg-base-100 link font-semibold link-primary rounded-box z-[1] w-44 p-2 shadow"
+              href="https://www.icann.org/resources/pages/epp-status-codes-2014-06-16-en"
+              target="_blank"
+            >
+              EPP Status Codes
+            </a>
+          </div>
         </li>
         <li className="flex gap-2">
           <h3 className="font-semibold">TRANSFER</h3>
-          <span className="bg-error text-neutral font-semibold text-xs px-1 rounded">
-            locked
-          </span>
+          {!domainStatus ? (
+            <NotAvailableMarker />
+          ) : (
+            <span
+              className={`${
+                transferCheck === 'active'
+                  ? 'bg-success'
+                  : transferCheck === 'pending transfer'
+                  ? 'bg-warning'
+                  : 'bg-error'
+              } text-neutral font-semibold text-xs px-1 rounded`}
+            >
+              {transferCheck}
+            </span>
+          )}
         </li>
         <div className="bg-secondary w-0.5 h-3 rounded-full"></div>
         <li className="flex gap-2">
           <h3 className="font-semibold">HAS CDN</h3>
-          <div className="bg-success text-neutral w-4 h-4 rounded-full font-semibold flex justify-center items-center">
-            <FaCheck size={8} />
-          </div>
+          {!domainStatus ? (
+            <NotAvailableMarker />
+          ) : (
+            <span className="bg-success text-neutral w-4 h-4 rounded-full font-semibold flex justify-center items-center">
+              <FaCheck size={8} />
+            </span>
+          )}
         </li>
         <li className="flex gap-2">
           <h3 className="font-semibold">WWW SSL</h3>
-          {wwwSsl ? (
-            <div className="bg-success text-neutral w-4 h-4 rounded-full font-semibold flex justify-center items-center">
-              <FaCheck size={8} />
-            </div>
+          {!domainStatus ? (
+            <NotAvailableMarker />
           ) : (
-            <div className="bg-error text-neutral w-4 h-4 rounded-full font-semibold flex justify-center items-center">
-              <IoClose size={12} />
-            </div>
+            <span
+              className={`${
+                wwwSsl ? 'bg-success' : 'bg-error'
+              } text-neutral w-4 h-4 rounded-full font-semibold flex justify-center items-center`}
+            >
+              {wwwSsl ? <FaCheck size={8} /> : <IoClose size={12} />}
+            </span>
           )}
         </li>
         <li className="flex gap-2">
           <h3 className="font-semibold">HAS WP</h3>
-          {hasWp ? (
-            <div className="bg-success text-neutral w-4 h-4 rounded-full font-semibold flex justify-center items-center">
-              <FaCheck size={8} />
-            </div>
+          {!domainStatus ? (
+            <NotAvailableMarker />
           ) : (
-            <div className="bg-error text-neutral w-4 h-4 rounded-full font-semibold flex justify-center items-center">
-              <IoClose size={12} />
-            </div>
+            <span
+              className={`${
+                hasWp ? 'bg-success' : 'bg-error'
+              } text-neutral w-4 h-4 rounded-full font-semibold flex justify-center items-center`}
+            >
+              {hasWp ? <FaCheck size={8} /> : <IoClose size={12} />}
+            </span>
           )}
         </li>
       </ul>
--- src/components/ThemeController.tsx
+++ src/components/ThemeController.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Theme } from '../enums/Theme.enum';
 import { useTheme } from '../providers/ThemeProvider';
 import { FaPaintRoller } from 'react-icons/fa';
+import { MdKeyboardArrowUp } from 'react-icons/md';
 
 const ThemeController: React.FC = () => {
   const { theme, changeTheme } = useTheme();
@@ -16,19 +17,11 @@ const ThemeController: React.FC = () => {
       >
         <FaPaintRoller />
         {theme}
-        <svg
-          width="12px"
-          height="12px"
-          className="inline-block h-2 w-2 fill-current opacity-60"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 2048 2048"
-        >
-          <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
-        </svg>
+        <MdKeyboardArrowUp size={16} />
       </div>
       <ul
         tabIndex={0}
-        className="dropdown-content bg-base-200 rounded-box z-[1] w-52 p-2 shadow-2xl text-neutral-content"
+        className="dropdown-content bg-base-200 rounded-box z-[1] p-2 shadow-md text-neutral-content"
       >
         {themes.map((theme: Theme, idx: number) => (
           <li key={idx}>
