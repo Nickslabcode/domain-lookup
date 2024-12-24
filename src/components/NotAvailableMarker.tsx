5de55f33eb6bee09d27391f59f083ceb6377b3e1
Nickslabcode <nicknenovski@gmail.com>
2024-12-24 17:18:42 +0200

    style(markers): update loading styles

--- src/components/NotAvailableMarker.tsx
+++ src/components/NotAvailableMarker.tsx
@@ -7,12 +7,14 @@ const NotAvailableMarker: React.FC<{
   return (
     <div
       className={cn(
-        'bg-secondary text-neutral font-semibold text-xs px-1 rounded flex items-center',
+        `${
+          loading ? 'bg-none' : 'bg-secondary'
+        } text-neutral font-semibold text-xs px-1 rounded flex items-center`,
         className
       )}
     >
       {loading ? (
-        <span className="loading loading-spinner loading-xs"></span>
+        <span className="loading loading-spinner loading-xs text-primary"></span>
       ) : (
         <span>N/A</span>
       )}
