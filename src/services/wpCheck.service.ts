c1d803d3c0be0b7dbadf761cd95215dba417d04f
Nickslabcode <nicknenovski@gmail.com>
2024-12-24 16:05:34 +0200

    refactor(wp check): replace CF worker WP check endpoint with Express one at AWS

--- src/services/wpCheck.service.ts
+++ src/services/wpCheck.service.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const isWordpressInstalled = async (domain: string) => {
   try {
     const response = await axios.get(
-      `https://domainlookup.nicknenovski.workers.dev/wp-check?domain=${domain}`,
+      `https://api-dl.nikola-nenovski.info/api/v1/wp-check?domain=${domain}`,
       {
         headers: {
           'Content-Type': 'application/json',
