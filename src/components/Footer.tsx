d31ce9b4240e3515a24d4875c20ed59fe6776d1f
Nickslabcode <nicknenovski@gmail.com>
2025-01-07 13:54:16 +0200

    feat(footer): add current year to footer
    Replace hardcoded year with fn returning the current year
    
    re #12

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import { Shortcut } from '../enums/Shortcut.enum';
 import AppVersion from './AppVersion';
 import ShortKeys from './ShortKeys';
@@ -5,11 +6,16 @@ import ThemeController from './ThemeController';
 import { FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
+  const getCurrentYear = useCallback((): string => {
+    const date = new Date();
+    return date.getFullYear().toString();
+  }, []);
+
   return (
     <div className="mt-10">
       <ShortKeys keys={Object.values(Shortcut)} />
       <div className="flex items-center text-sm justify-around text-base-content">
-        <p>© 2024 Nikola Nenovski. All right reserved.</p>
+        <p>© {getCurrentYear()} Nikola Nenovski. All rights reserved.</p>
         <div className="flex items-center gap-2 cursor-default">
           <AppVersion />
           <a
