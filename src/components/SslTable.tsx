14088a6fcf431bc8c183df776bbe6994ede3ffa1
Nickslabcode <nicknenovski@gmail.com>
2025-01-07 18:44:05 +0200

    refactor(loading): implement loading component for the tables

--- src/components/SslTable.tsx
+++ src/components/SslTable.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo } from 'react';
 import { getDays } from '../helpers/getDays';
 import { SslStatus } from '../enums/SslStatus.enum';
+import Loading from './Loading';
 
 const SslTable: React.FC<{
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -34,9 +35,7 @@ const SslTable: React.FC<{
   return (
     <div className="flex lg:min-h-80 lg:max-h-96 h-full flex-col break-words shadow-md p-4 rounded-lg cursor-default overflow-y-auto border border-neutral">
       {loading ? (
-        <div className="flex justify-center items-center h-full">
-          <span className="loading loading-spinner text-primary"></span>
-        </div>
+        <Loading />
       ) : !content ? (
         <div className="flex justify-center items-center h-full">
           <p>No SSL certificate found.</p>
