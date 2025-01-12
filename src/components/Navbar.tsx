b826f0149aec132d1976e68cb55dac42628a68dc
Nikola Nenovski <nicknenovski@gmail.com>
2025-01-12 15:50:12 +0200

    fix(navbar): fix navbar width issue on larger screens
    Updated the width value and added left and right to be 0 for the fixed navbar position to ensure
    that it stays centered no matter the screen size
    
    fix #10

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import SearchForm from './SearchForm';
 
 const Navbar = () => {
   return (
-    <div className="fixed w-full flex items-center gap-4 py-4 justify-center backdrop-blur-md z-10">
+    <div className="fixed left-0 right-0 w-full flex items-center gap-4 py-4 justify-center backdrop-blur-md z-10">
       <Logo />
       <SearchForm />
     </div>
